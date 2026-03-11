5cf7fb58ef72b139c1973987874b59015500e69a
Kloboukov <kloboukus@post.cz>
2026-03-11 17:55:16 +0100

    the line is looks now that Frederick and Odra dont´t say that i muss something chang. But the color fond and frame line are still bad

--- timeline/data.js
+++ timeline/data.js
@@ -1,12 +1,79 @@
 const data = [
-  { date: "1918-10-28", country: "CZ", CZtitle: "Vznik Československa",      DEtitle: "Gründung der Tschechoslowakei",        ENtitle: "Creation of Czechoslovakia",        url: "cz_first.html" },
-  { date: "1918-11-09", country: "DE", CZtitle: "Vyhlášení Výmarské rep.",   DEtitle: "Ausrufung der Weimarer Republik",      ENtitle: "Proclamation of the Weimar Republic", url: "de_republika.html" },
-  { date: "1919-08-11", country: "DE", CZtitle: "Výmarská ústava",           DEtitle: "Weimarer Verfassung",                  ENtitle: "Weimar Constitution",               url: "de_ustava.html" },
-  { date: "1920-02-29", country: "CZ", CZtitle: "Ústava ČSR",                DEtitle: "Verfassung der Tschechoslowakei",      ENtitle: "Constitution of Czechoslovakia",    url: "cz_ustava.html" },
-  { date: "1923-11-08", country: "DE", CZtitle: "Pivní puč",                 DEtitle: "Hitlers Putschversuch (Bierkellerputsch)", ENtitle: "Beer Hall Putsch",              url: "de_puc.html" },
-  { date: "1925-10-16", country: "DE", CZtitle: "Locarnské smlouvy",         DEtitle: "Locarno-Verträge",                     ENtitle: "Locarno Treaties",                  url: "de_locarno.html" },
-  { date: "1929-10-01", country: "CZ", CZtitle: "Hospodářská krize v ČSR",   DEtitle: "Weltwirtschaftskrise in der Tschechoslowakei", ENtitle: "Great Depression in Czechoslovakia", url: "cz_krize.html" },
-  { date: "1933-01-30", country: "DE", CZtitle: "Hitler říšský kancléř",     DEtitle: "Hitler wird Reichskanzler",            ENtitle: "Hitler becomes Reich Chancellor",   url: "de_hitler.html" },
-  { date: "1935-12-18", country: "CZ", CZtitle: "Beneš prezidentem",         DEtitle: "Edvard Beneš wird Präsident",         ENtitle: "Edvard Beneš becomes President",   url: "cz_benes.html" },
-  { date: "1938-09-29", country: "CZ", CZtitle: "Mnichovská dohoda",         DEtitle: "Münchner Abkommen",                    ENtitle: "Munich Agreement",                  url: "cz_mnichov.html" },
+  {
+    date: "1918-11-14",
+    country: "CZ",
+    CZtitle: "vznik Rev. nár. shromáždění",
+    CZdescription: "Vznik Revolučního národního shromáždění, Masaryk prezidentem",
+    DEtitle: "Gründung der Revolutionären Nationalversammlung",
+    DEdescription: "Gründung der Revolutionären Nationalversammlung, Masaryk Präsident",
+    ENtitle: "Creation of the Revolutionary National Assembly",
+    ENdescription: "Creation of Revolutionary National Assembly, Masaryk as President",
+    url: "19181114.html"
+  },
+  {
+    date: "1918-12-01",
+    country: "CZ",
+    CZtitle: "8h pracovní doba",
+    CZdescription: "Zákon o osmihodinové pracovní době",
+    DEtitle: "8-Stunden-Arbeitstag",
+    DEdescription: "Gesetz über den Acht-Stunden-Arbeitstag",
+    ENtitle: "8-hour workday",
+    ENdescription: "Law on eight-hour workday",
+    url: "19181201.html"
+  },
+  {
+    date: "1919-06-15",
+    country: "CZ",
+    CZtitle: " volby i pro ženy",
+    CZdescription: "Komunální volby, ženy mohou poprvé volit a být voleny",
+    DEtitle: "Kommunalwahlen auch für Frauen",
+    DEdescription: "Kommunalwahlen, Frauen dürfen erstmals wählen und gewählt werden",
+    ENtitle: "Municipal elections also for women",
+    ENdescription: "Municipal elections, women can vote and be elected for the first time",
+    url: "19190615.html"
+  },
+  {
+    date: "1920-02-29",
+    country: "CZ",
+    CZtitle: "schválení ústavy ",
+    CZdescription: "Schválení finální ústavy ČSR (rovnoprávnost žen)",
+    DEtitle: "Verabschiedung der Verfassung (Gleichberechtigung der Frauen)",
+    DEdescription: "Verabschiedung der endgültigen Verfassung der Tschechoslowakei (Gleichberechtigung der Frauen)",
+    ENtitle: "Approval of constitution (women's equality)",
+    ENdescription: "Approval of final Czechoslovak constitution (women's equality)",
+    url: "19200229.html"
+  },
+  {
+    date: "1920-04-18",
+    country: "CZ",
+    CZtitle: "volby do poslanecké sněmovny",
+    CZdescription: "Volby do poslanecké sněmovny",
+    DEtitle: "Wahlen zum Abgeordnetenhaus",
+    DEdescription: "Wahlen zum Abgeordnetenhaus",
+    ENtitle: "Elections to the Chamber of Deputies",
+    ENdescription: "Elections to the Chamber of Deputies",
+    url: "19200418.html"
+  },
+  {
+    date: "1920-04-25",
+    country: "CZ",
+    CZtitle: "volby do senátu",
+    CZdescription: "Volby do senátu",
+    DEtitle: "Wahlen zum Senat",
+    DEdescription: "Wahlen zum Senat",
+    ENtitle: "Elections to the Senate",
+    ENdescription: "Elections to the Senate",
+    url: "19200425.html"
+  },
+  {
+    date: "1920-05-27",
+    country: "CZ",
+    CZtitle: "konec Rev. nár. shromáždění",
+    CZdescription: "Konec činnosti Revolučního národního shromáždění",
+    DEtitle: "Ende der Revolutionären Nationalversammlung",
+    DEdescription: "Ende der Tätigkeit der Revolutionären Nationalversammlung",
+    ENtitle: "End of the Revolutionary National Assembly",
+    ENdescription: "End of activity of the Revolutionary National Assembly",
+    url: "19200527.html"
+  }
 ];
