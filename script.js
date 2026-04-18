2e43240250f003e4dfe45bea06984e9f8d28be2f
Kloboukov <kloboukus@post.cz>
2026-04-18 11:15:26 +0200

    Add function to load and display image from file input

--- script.js
+++ script.js
@@ -258,3 +258,21 @@ function parseSourceLinks(container) {
     container.appendChild(sourcesList);
   }
 }
+
+function loadImageFromFile(){
+  let fileInput = document.getElementById("file");
+  let files = fileInput.files;
+  if (files.length == 0) {
+    return;
+  }
+  let file = files[0];
+  fileReader = new FileReader();
+  fileReader.onload = function(e){
+    document.getElementById("image").src = e.target.result;
+    document.getElementById("imageHidden").src = e.target.result;
+  };
+  fileReader.onerror = function () {
+    console.warn('oops, something went wrong.');
+  };
+  fileReader.readAsDataURL(file);
+}
